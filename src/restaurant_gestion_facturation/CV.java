023d6faa479205dd705f2e276f95deafc915133f
Yasser Soliman <yassersoliman_1@hotmail.com>
2020-02-06 10:30:57 -0500

    Creation de methode affiche

--- src/restaurant_gestion_facturation/CV.java
+++ src/restaurant_gestion_facturation/CV.java
@@ -23,6 +23,19 @@ public class CV {
 
 	}
 	
-	
+	public void affiche() {
+		String competencesStr = "";
+		
+		for (String comp : competences) {
+			competencesStr += "\t*" + comp + "\n";
+		}
+		
+		System.out.println("Nom : " + this.nom 
+						+ "\nPrenom : " + this.prenom
+						+ "\nFormation : " + this.formation
+						+ "\nExpérience de travail : " + this.expTravail + " an(s)"
+						+ "\nCompétences : \n" + competencesStr
+						+ "Attentes vis à vis le cours 4B4 : " + this.attentes);
+	} 
 
 }
