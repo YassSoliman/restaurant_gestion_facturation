d925b303bda2f8d4ed49ea3a310d3ce20aaa01f7
Yasser Soliman <yassersoliman_1@hotmail.com>
2020-02-06 10:40:00 -0500

    Ajout de CV de chaque membres et affichage de CV

--- src/restaurant_gestion_facturation/CV.java
+++ src/restaurant_gestion_facturation/CV.java
@@ -19,23 +19,28 @@ public class CV {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("Bienvenue chez Barette!\n");
 		
-
+		CV cvYasser = new CV("Soliman", "Yasser", "DEC en informatique", 50, new String[] {"HTML", "Java", "JavaScript", "C#"}, "Apprendre beaucoup sur JUnit." );
+		CV cvAnass = new CV("Soliman", "Anass", "DEC en informatique", 87, new String[] {"CSS", "MySQL", "TypeScript", "C#"}, "Maitriser le logiciel de gestion de vesrion GIT." );
+		
+		cvYasser.affiche();
+		cvAnass.affiche();
 	}
 	
 	public void affiche() {
 		String competencesStr = "";
 		
 		for (String comp : competences) {
-			competencesStr += "\t*" + comp + "\n";
+			competencesStr += "\t\t\t\t* " + comp + "\n";
 		}
 		
-		System.out.println("Nom : " + this.nom 
-						+ "\nPrenom : " + this.prenom
-						+ "\nFormation : " + this.formation
-						+ "\nExpérience de travail : " + this.expTravail + " an(s)"
+		System.out.println("Nom : \t\t\t\t" + this.nom 
+						+ "\nPrenom : \t\t\t" + this.prenom
+						+ "\nFormation : \t\t\t" + this.formation
+						+ "\nExpérience de travail : \t" + this.expTravail + " an(s)"
 						+ "\nCompétences : \n" + competencesStr
-						+ "Attentes vis à vis le cours 4B4 : " + this.attentes);
+						+ "Attentes envers 4B4 : \t\t" + this.attentes + "\n");
 	} 
 
 }
