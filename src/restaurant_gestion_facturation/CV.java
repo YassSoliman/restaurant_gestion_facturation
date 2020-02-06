ccc4d683f2fbd3daa07ebfb08eb74e0f4ab76d89
Yasser Soliman <yassersoliman_1@hotmail.com>
2020-02-06 10:25:32 -0500

    Ajout de constructeur et attributs

---
+++ src/restaurant_gestion_facturation/CV.java
@@ -0,0 +1,28 @@
+package restaurant_gestion_facturation;
+
+public class CV {
+	
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int expTravail;
+	private String[] competences;
+	private String attentes;
+	
+	public CV(String nom, String prenom, String formation, int expTravail, String[] competences, String attentes) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.expTravail = expTravail;
+		this.competences = competences;
+		this.attentes = attentes;
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+	
+	
+
+}
