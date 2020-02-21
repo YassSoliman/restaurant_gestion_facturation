5c888ede1a85655befcc3b93e5b0d280ac5805da
Anass_Soliman <anass_soliman@outlook.com>
2020-02-21 15:38:43 -0500

    Calcul de facture pour chaque client

--- src/restaurant_gestion_facturation/Client.java
+++ src/restaurant_gestion_facturation/Client.java
@@ -1,8 +1,11 @@
 package restaurant_gestion_facturation;
 
+import java.util.List;
+
 public class Client {
 
 	private String nomClient;
+	private List<Commande> listeCommande;
 	
 	public Client(String nomClient) {
 		this.setNomClient(nomClient);
@@ -14,5 +17,29 @@ public class Client {
 
 	public void setNomClient(String nomClient) {
 		this.nomClient = nomClient;
+	}
+	
+	public void ajouterCommande(Commande commande) {
+		listeCommande.add(commande);
+	}
+	
+	public List<Commande> getListeCommande() {
+		return listeCommande;
+	}
+	
+	public double calculerFacture() {
+		double prixFacture = 0;
+		
+		for (int i = 0; i < listeCommande.size(); i++) {
+			prixFacture += listeCommande.get(i).getPlatCommander().getPrixPlat() * 
+					listeCommande.get(i).getQteCommande();
+		}
+		
+		return prixFacture;
+	}
+	
+	@Override
+	public String toString() {
+		return this.nomClient;
 	}
 }
