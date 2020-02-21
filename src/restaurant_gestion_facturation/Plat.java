189eaf908d26dd09217d84c2981a567e8e1b0c5a
Yasser Soliman <yassersoliman_1@hotmail.com>
2020-02-21 14:56:44 -0500

    Ajout de getters et setters pour la classe Plat

--- src/restaurant_gestion_facturation/Plat.java
+++ src/restaurant_gestion_facturation/Plat.java
@@ -1,12 +1,29 @@
 package restaurant_gestion_facturation;
 
 public class Plat {
-	String nomPlat;
-	double prixPlat;
-
+	private String nomPlat;
+	
+	private double prixPlat;
+	
 	public Plat(String nom, double prix) {
 		this.nomPlat = nom;
 		this.prixPlat = prix;
+	}
+	
+	public String getNomPlat() {
+		return nomPlat;
+	}
+
+	public void setNomPlat(String nomPlat) {
+		this.nomPlat = nomPlat;
+	}
+
+	public double getPrixPlat() {
+		return prixPlat;
+	}
+
+	public void setPrixPlat(double prixPlat) {
+		this.prixPlat = prixPlat;
 	}
 	
 	// Methode pour afficher le nom du plat avec son prix
