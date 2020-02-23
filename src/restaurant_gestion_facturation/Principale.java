c8942184bf5d90e621b7017b60cdac25fc1217cf
Anass_Soliman <anass_soliman@outlook.com>
2020-02-22 20:17:21 -0500

    Methode pour ecriture de fichier

--- src/restaurant_gestion_facturation/Principale.java
+++ src/restaurant_gestion_facturation/Principale.java
@@ -1,10 +1,34 @@
 package restaurant_gestion_facturation;
 
+import java.io.*;
+
+import outilsjava.OutilsFichier;
+
 public class Principale {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
+	
+	
+	//Ecriture du fichier 
+	private BufferedWriter ecrireFichier( String nomFichier, Client client ) {
 
+		BufferedWriter ficEcriture = null;
+
+		ficEcriture = OutilsFichier.ouvrirFicTexteEcriture(nomFichier);
+
+		if ( ficEcriture != null ) {
+			
+			try {
+				ficEcriture.write("Bienvenue chez Barette!\nFactures:\n" + client.getNomClient() + " " + client.calculerFacture() + "$");
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+
+			OutilsFichier.fermerFicTexteEcriture( ficEcriture, nomFichier );
+		}
+		
+		return ficEcriture;
+	}
 }
