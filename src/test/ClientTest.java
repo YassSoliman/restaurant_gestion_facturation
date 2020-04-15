35cde49a4b8a945b06e6025a9a52bacd34f0c04f
mosoliman <solimohammad@hotmail.com>
2020-04-15 19:31:04 -0400

    Ajout du test pour la methode validerClient() dans le cas de test jUnit ClientTest.java

--- src/test/ClientTest.java
+++ src/test/ClientTest.java
@@ -10,5 +10,15 @@ import main.Plat;
 
 public class ClientTest {
 
-	
+	@Test
+	public void testValiderClient() {
+		Client client = new Client();
+		Plat plat = null;
+		Commande commande = new Commande(client, plat, 0);
+		client.ajouterCommande(commande);
+			
+		String chaineValidation = "Le client " + client.getNomClient() + " n'existe pas.";
+		
+		assertEquals(chaineValidation, Client.validerClient(client));
+	}
 }
--- src/test/PrincipaleTest.java
+++ src/test/PrincipaleTest.java
@@ -10,15 +10,4 @@ import main.Plat;
 
 public class PrincipaleTest {
 
-	@Test
-	public void testValiderClient() {
-		Client client = new Client();
-		Plat plat = null;
-		Commande commande = new Commande(client, plat, 0);
-		client.ajouterCommande(commande);
-			
-		String chaineValidation = "Le client " + client.getNomClient() + " n'existe pas.";
-		
-		assertEquals(chaineValidation, Client.validerClient(client));
-	}
 }
