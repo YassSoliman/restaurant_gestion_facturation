fde8d63e9c44c1b3a114627c1a2e68e86b8338d2
Anass_Soliman <anass_soliman@outlook.com>
2020-04-15 17:35:25 -0400

    Précision des spécifications et modification de la classe Facture

--- src/main/Facture.java
+++ src/main/Facture.java
@@ -3,14 +3,30 @@ package main;
 public class Facture {
 	
 	private Client client;
+	private double prix;
 	
 	public Client getClient() {
 		return this.client;
 	}
+	
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	
+	public double getPrix() {
+		return this.prix;
+	}
+	
+	public Facture() {}
+	
+	public Facture(Client client, double prix) {
+		this.client = client;
+		setPrix(prix);
+	}
 
 	public double calculerFacture() {
 		double prixFacture = 0;
-		
+		//TODO ajouter TPS TVQ
 		for (int i = 0; i < client.getListeCommande().size(); i++) {
 			prixFacture += client.getListeCommande().get(i).getPlatCommander().getPrixPlat() * 
 					client.getListeCommande().get(i).getQteCommande();
@@ -19,7 +35,8 @@ public class Facture {
 		return prixFacture;
 	}
 
-	public String afficherFacture() {
+	@Override
+	public String toString() {
 		String facture = "";
 		
 		if (calculerFacture() != 0) {
