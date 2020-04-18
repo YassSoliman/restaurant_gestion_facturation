bfd16ead144f4651f8cd8fc3bd6a72051f5b00ad
Yasser Soliman <yassersoliman_1@hotmail.com>
2020-04-17 22:37:42 -0400

    PlatTest test unitaire corriger

--- src/test/PlatTest.java
+++ src/test/PlatTest.java
@@ -1,11 +1,11 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import main.Client;
-import main.Commande;
 import main.Plat;
 
 public class PlatTest {
@@ -22,30 +22,8 @@ public class PlatTest {
 	
 	@Test
 	public void testCreerPlat() {
-		String contenu = "Clients :\n" + 
-				"Roger\n" + 
-				"Céline\n" + 
-				"Steeve\n" + 
-				"Plats :\n" + 
-				"Poutine 10.5\n" + 
-				"Frites 2.5\n" + 
-				"Repas_Poulet 15.75\n" + 
-				"Commandes :\n" + 
-				"Roger Poutine 1\n" + 
-				"Céline Frites 2\n" + 
-				"Céline Repas_Poulet 1\n" + 
-				"Fin";
-		// Cas creerPlat fonctionnel
 		Plat.getListePlats().clear();
-		Plat.creerPlat(contenu);
-		Plat plat = new Plat();
-		plat.setNomPlat("Frites");
-		plat.setPrixPlat(2.5);		
-		
-		assertTrue(Plat.getListePlats().contains(plat));
-		
 		// Cas contenu vide
-		Plat.getListePlats().clear();
 		Plat.creerPlat("");
 		assertTrue(Plat.getListePlats().isEmpty());
 	}
