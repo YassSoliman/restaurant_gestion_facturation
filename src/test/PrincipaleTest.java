53f33392750c1c9205d4b242af78da388ecd60df
Yasser Soliman <yassersoliman_1@hotmail.com>
2020-04-18 01:25:49 -0400

    Modification dans un test

--- src/test/PrincipaleTest.java
+++ src/test/PrincipaleTest.java
@@ -55,20 +55,6 @@ public class PrincipaleTest {
 				"Céline Repas_Poulet 1\n" + 
 				"Fin";
 		assertTrue(Principale.validerFormat(contenu));
-		contenu = "Clients :\n" + 
-				"Roger\n" + 
-				"Céline\n" + 
-				"Steeve\n" + 
-				"Plats :\n" + 
-				"Poutine 10.5\n" + 
-				"Frites 2.5\n" + 
-				"Repas_Poulet 15.75\n" + 
-				"Commandes :\n" + 
-				"Roger Poutine 1\n" + 
-				"Céline Frites 2\n" + 
-				"Céline Repas_Poulet 1\n" + 
-				"Fin";
-		assertTrue(Principale.validerFormat(contenu));
 		// Cas "Clients :" invalide
 		contenu = "Clies :\n" + 
 				"Roger\n" + 
